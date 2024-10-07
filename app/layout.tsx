52c2b8f8b3b030781e40bda1a2bd947715a9ffef
Siddesh <siddeshsankhya@gmail.com>
2024-10-07 14:02:38 +0530

    remove - fe files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils";
-import Providers from "@/providers/privy-provider";
-import Navbar from "@/components/ui/navbar";
-import Footer from "@/components/ui/footer";
-import {UserContextProvider} from "@/providers/user-context-provider";
-import GoogleAnalytics from "@/components/google-analytics";
 
 const inter = Inter({subsets: ["latin"], variable: "--font-sans"});
 
@@ -21,20 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleAnalytics />
       <body
         className={cn(
           "flex flex-col min-h-screen bg-background font-sans antialiased",
           inter.variable
         )}
       >
-        <Providers>
-          <UserContextProvider>
-            <Navbar />
-            <section className="flex-grow"> {children}</section>
-          </UserContextProvider>
-        </Providers>
-        <Footer />
+        <section className="flex-grow"> {children}</section>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,10 @@
 "use client";
-import AiValueComponent from "@/components/ai-value-component";
-import {Button} from "@/components/ui/button";
-import SpectrumCard from "@/components/ui/spectrum-card";
-import ValuesWordCloud from "@/components/word-cloud";
-import {SPECTRUMS} from "@/constants";
-import {useUserContext} from "@/providers/user-context-provider";
-import {usePrivy} from "@privy-io/react-auth";
-import Image from "next/image";
+
+import {useEffect} from "react";
 
 export default function Home() {
-  const {userInfo, isLoading} = useUserContext();
-  const {authenticated, ready, login} = usePrivy();
-  return (
-    <main className="mt-8">
-      {!isLoading && (
-        <>
-          {authenticated && <ValuesWordCloud />}
-          {authenticated && <AiValueComponent />}
-          {ready && !authenticated && (
-            <div className="flex flex-col items-center px-6 mt-12">
-              <div
-                className={`grid md:grid-cols-3 gap-2 my-4 mb-12 ${
-                  window.innerWidth > 768 ? "grid-cols-2" : "grid-cols-1"
-                }`}
-              >
-                {Array(window.innerWidth > 768 ? 3 : 1)
-                  .fill(1)
-                  .map((_, index) => (
-                    <SpectrumCard
-                      key={index}
-                      name={SPECTRUMS[index]}
-                      score={Math.random() * 100}
-                      description="Description"
-                    />
-                  ))}
-              </div>
-              <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl text-center">
-                Collect your Values!
-              </h1>
-              <p className="leading-7 [&:not(:first-child)]:mt-6 text-center">
-                Mint your Values onchain.
-              </p>
-              <Button
-                variant="default"
-                onClick={login}
-                disabled={!ready || authenticated}
-                className="my-4"
-              >
-                Get Started
-              </Button>
-            </div>
-          )}
-        </>
-      )}
-
-      {ready && isLoading && (
-        <div className="flex flex-col gap-2 items-center justify-center h-screen">
-          <div className="animate-spin rounded-full h-14 w-14 border-t-2 border-b-2 border-primary"></div>
-          <h4 className="scroll-m-20 text-lg font-light tracking-tight">
-            Setting up your Profile..
-          </h4>
-        </div>
-      )}
-
-      {!ready && (
-        <div className="fixed inset-0 flex flex-col items-center justify-center bg-opacity-50 backdrop-filter backdrop-blur-lg">
-          <div className="animate-spin rounded-full h-14 w-14 border-t-2 border-b-2 border-primary"></div>
-          <h4 className="scroll-m-20 text-lg font-light tracking-tight">
-            Loading...
-          </h4>
-        </div>
-      )}
-    </main>
-  );
+  useEffect(() => {
+    window.location.href = "https://app.valuesdao.io";
+  }, []);
+  return <main></main>;
 }
