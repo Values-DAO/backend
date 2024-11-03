c867d8546a4a84bf0fb7984e159383be636ce890
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-11-04 03:52:36 +0530

    fix bug in schema

--- models/trustPool.ts
+++ models/trustPool.ts
@@ -1,39 +1,45 @@
 import mongoose, {models, Schema} from "mongoose";
 
 const trustPoolSchema = new Schema({
-	// name, description, logo, values, telegram link, twitter handle, owners, members
-	name: {
-		type: String,
-		required: true
-	},
-	description: {
-		type: String,
-	},
-	logo: {
-		type: String,
-	},
-	telegramLink: {
-		type: String,
-	},
-	twitterHandle: {
-		type: String,
-	},
-	organizerTwitterHandle: {
-		type: String,
-	},
-	owners: [{
-		type: Schema.Types.ObjectId,
-		ref: "Users",
-		required: true
-	}],
-	members: {
-		type: [{
-			type: Schema.Types.ObjectId,
-			ref: "Users"
-		}],
-		default: []
-	}
-})
+  // name, description, logo, values, telegram link, twitter handle, owners, members
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  logo: {
+    type: String,
+  },
+  telegramLink: {
+    type: String,
+  },
+  twitterHandle: {
+    type: String,
+  },
+  organizerTwitterHandle: {
+    type: String,
+  },
+  owners: {
+    type: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+		required: true,
+  },
+  members: {
+    type: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+    default: [],
+  },
+});
 
 const TrustPools = models.TrustPools || mongoose.model("TrustPools", trustPoolSchema);
 export default TrustPools
