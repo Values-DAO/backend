44fdd8c92386c9b361830c54ca209359a7320309
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-11-08 16:36:31 +0530

    fix farcaster username insert

--- lib/get-farcaster-user.ts
+++ lib/get-farcaster-user.ts
@@ -1,5 +1,8 @@
 import {AIRSTACK_API_URL} from "@/constants";
 import {GraphQLClient} from "graphql-request";
+import rateLimit from "axios-rate-limit";
+import axios from "axios";
+
 type FarcasterSocialData = {
   Socials: {
     Social: {
@@ -8,6 +11,14 @@ type FarcasterSocialData = {
     }[];
   };
 };
+
+type EngagementResult = {
+  fid: number;
+  username: string;
+  rank: number;
+  score: number;
+  percentile: number;
+}
 
 export async function getFarcasterUser(fid: number) {
   const query = `query GetFarcasterUsername {
@@ -36,29 +47,60 @@ export async function getFarcasterUser(fid: number) {
   }
 }
 
+
+const axiosInstance = rateLimit(axios.create(), {
+  maxRequests: 2, // Maximum 3 requests per second
+  perMilliseconds: 1000, // Reset limit every 1 second
+});
+
+// OpenRank
 export async function getFarcasterUsernameFromFID(fid: number): Promise<string | null> {
-  const query = `query GetFarcasterUsername {
-  Socials(
-    input: {filter: {dappName: {_eq: farcaster}, userId: {_eq: "${fid}"}}, blockchain: ethereum}
-  ) {
-    Social {
-      profileHandle
-      profileName
-    }
-  }
-}`;
-
-  const graphQLClient = new GraphQLClient(AIRSTACK_API_URL, {
-    headers: {
-      Authorization: process.env.NEXT_PUBLIC_AIRSTACK_API_KEY || "",
-    },
-  });
-
   try {
-    const data: FarcasterSocialData = await graphQLClient.request(query);
+    const response = await axiosInstance.post<{
+      result: EngagementResult[];
+    }>("https://graph.cast.k3l.io/scores/global/engagement/fids?engagement_type=1.0&lite=true", [fid]);
 
-    return data.Socials.Social[0].profileName;
+    const engagementData = response.data.result;
+    if (engagementData && engagementData.length > 0) {
+      return engagementData[0].username;
+    }
+    return null;
   } catch (error) {
+    console.error("Error fetching Farcaster username:", error);
     return null;
   }
 }
+
+// Airstack
+// export async function getFarcasterUsernameFromFID(fid: number): Promise<string | null> {
+//   const query = `query GetFarcasterUsername {
+//     Socials(
+//       input: {filter: {dappName: {_eq: farcaster}, userId: {_eq: "${fid}"}}, blockchain: ethereum}
+//     ) {
+//       Social {
+//         profileHandle
+//         profileName
+//       }
+//     }
+//   }`;
+
+//   try {
+//     const { data }: { data: FarcasterSocialData } = await axiosInstance.post(
+//       AIRSTACK_API_URL,
+//       {
+//         query,
+//       },
+//       {
+//         headers: {
+//           Authorization: process.env.NEXT_PUBLIC_AIRSTACK_API_KEY || "",
+//         },
+//       }
+//     );
+    
+//     console.log(data.Socials);
+
+//     return data.Socials.Social[0].profileName;
+//   } catch (error) {
+//     return null;
+//   }
+// }
--- lib/insert-farcaster-username.ts
+++ lib/insert-farcaster-username.ts
@@ -1,39 +1,45 @@
 import connectToDatabase from "@/lib/connect-to-database";
 import Users from "@/models/user";
 import {getFarcasterUsernameFromFID} from "@/lib/get-farcaster-user";
+import { resolve } from "path";
 
 export const insertFarcasterUsername = async () => {
 	try {
 		await connectToDatabase()
 
 		let skip = 0
-		const BATCH_SIZE = 100
+		const BATCH_SIZE = 10
 		let users;
 
 		do {
-			users = await Users.find({farcasterUsername: {$exists: false}})
-				.skip(skip)
-				.limit(100)
+			users = await Users.find({ farcasterUsername: { $exists: false } })
+        .skip(skip)
+        .limit(BATCH_SIZE);
 
 			for (const user of users) {
-				const {fid} = user
-
-				const farcasterUsername = await getFarcasterUsernameFromFID(fid)
-
-				if (!farcasterUsername) {
-					console.log(`No farcaster username found for user ${fid}`)
+				const {fid, farcasterUsername} = user
+				
+				if (!fid) {
+					console.log(`No fid found for user ${fid}`)
+					continue
+				}
+				
+				if (farcasterUsername) {
+					console.log(`User ${fid} already has farcaster username ${farcasterUsername}`)
 					continue
 				}
 
-				if (farcasterUsername) {
-					await Users.updateOne({fid}, {$set: {farcasterUsername}})
-					console.log(`Updated user ${fid} with farcaster username ${farcasterUsername}`)
+				const fetchedFarcasterUsername = await getFarcasterUsernameFromFID(fid)
+
+				if (fetchedFarcasterUsername) {
+					const res = await Users.updateOne({fid}, {$set: {farcasterUsername: fetchedFarcasterUsername}})
+					console.log(`Updated user ${fid} with farcaster username ${fetchedFarcasterUsername}`)
 				} else {
 					console.log(`No farcaster username found for user ${fid}`)
 				}
 
-				skip += BATCH_SIZE
 			}
+			skip += BATCH_SIZE
 		} while (users.length > 0)
 
 		console.log("Finished inserting farcaster usernames")
