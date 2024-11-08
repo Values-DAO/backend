9b1fbf4798cccfda62c9284cad64502393c78ee2
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-11-09 00:31:08 +0530

    update next.config.mjs to avoid cors

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
         source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: process.env.NODE_ENV === "production" ? "https://staging-api.valuesdao.io" : "http://localhost:3001" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_APP_ENV === "staging" ? "https://staging.valuesdao.io" : "http://localhost:3001" }, // replace this your actual origin
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           {
             key: "Access-Control-Allow-Headers",
