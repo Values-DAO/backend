c62e7cce9524d01f687f331c0890ad583105a4da
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-11-10 12:55:58 +0530

    fix trustpools being static

--- app/trustpools/route.ts
+++ app/trustpools/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import connectToDatabase from "@/lib/connect-to-database";
 import TrustPools from "@/models/trustPool";
@@ -6,54 +8,29 @@ export async function GET(req: Request) {
   try {
     await connectToDatabase();
 
-    // Find the most recent 40 trustpools
+    // find most recent 40 trustpools
     const trustpools = await TrustPools.find({}).sort({ createdAt: -1 }).limit(40);
 
     if (trustpools.length === 0) {
-      return NextResponse.json(
-        {
-          error: "Trustpools not found",
-          message: "Trustpools not found",
-        },
-        {
-          status: 404,
-          headers: {
-            "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-            Pragma: "no-cache",
-            Expires: "0",
-          },
-        }
-      );
+      return NextResponse.json({
+        error: "Trustpools not found",
+        message: "Trustpools not found",
+      });
     }
 
-    return NextResponse.json(
-      {
-        status: 200,
-        message: "Trustpools found successfully",
-        data: trustpools,
-      },
-      {
-        headers: {
-          "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-          Pragma: "no-cache",
-          Expires: "0",
-        },
-      }
-    );
+    NextResponse.headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+    NextResponse.headers.set("Expires", "0");
+    NextResponse.headers.set("Surrogate-Control", "no-store");
+
+    return NextResponse.json({
+      status: 200,
+      message: "Trustpools found successfully",
+      data: trustpools,
+    });
   } catch (error) {
-    return NextResponse.json(
-      {
-        error: error,
-        message: "Internal server error",
-      },
-      {
-        status: 500,
-        headers: {
-          "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-          Pragma: "no-cache",
-          Expires: "0",
-        },
-      }
-    );
+    return NextResponse.json({
+      error: error,
+      message: "Internal server error",
+    });
   }
 }
