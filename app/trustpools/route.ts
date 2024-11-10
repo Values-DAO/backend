7b620a43d464e07c06742d26307f95d6c3ec502a
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-11-10 12:52:11 +0530

    disable /trustpool caching

--- app/trustpools/route.ts
+++ app/trustpools/route.ts
@@ -1,32 +1,59 @@
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 import connectToDatabase from "@/lib/connect-to-database";
 import TrustPools from "@/models/trustPool";
 
 export async function GET(req: Request) {
-	try {
-		await connectToDatabase()
+  try {
+    await connectToDatabase();
 
-		// find most recent 40 trustpools
-		const trustpools = await TrustPools.find({})
-			.sort({ createdAt: -1 })
-			.limit(40);
+    // Find the most recent 40 trustpools
+    const trustpools = await TrustPools.find({}).sort({ createdAt: -1 }).limit(40);
 
-		if (trustpools.length === 0) {
-			return NextResponse.json({
-				error: "Trustpools not found",
-				message: "Trustpools not found",
-			})
-		}
+    if (trustpools.length === 0) {
+      return NextResponse.json(
+        {
+          error: "Trustpools not found",
+          message: "Trustpools not found",
+        },
+        {
+          status: 404,
+          headers: {
+            "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+            Pragma: "no-cache",
+            Expires: "0",
+          },
+        }
+      );
+    }
 
-		return NextResponse.json({
-			status: 200,
-			message: "Trustpools found successfully",
-			data: trustpools
-		})
-	} catch (error) {
-		return NextResponse.json({
-			error: error,
-			message: "Internal server error",
-		})
-	}
+    return NextResponse.json(
+      {
+        status: 200,
+        message: "Trustpools found successfully",
+        data: trustpools,
+      },
+      {
+        headers: {
+          "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error,
+        message: "Internal server error",
+      },
+      {
+        status: 500,
+        headers: {
+          "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      }
+    );
+  }
 }
