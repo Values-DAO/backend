f648b4ae0a39d88f801999c9291f7592a6418516
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-12-17 13:22:41 +0530

    fix cors error

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
         source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_APP_ENV === "prod" ? "https://app.valuesdao.io" : process.env.NEXT_PUBLIC_APP_ENV === "staging" ? "http://staging.valuesdao.io" : "http://localhost:3001" }, 
+          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_APP_ENV === "prod" ? "https://app.valuesdao.io" : process.env.NEXT_PUBLIC_APP_ENV === "staging" ? "https://staging.valuesdao.io" : "http://localhost:3001" }, 
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           {
             key: "Access-Control-Allow-Headers",
